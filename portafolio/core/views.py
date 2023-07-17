fa990ecbcfe2198172e938ca1dcae8bc8c35744a
unknown <rpalacioso12@gamil.com>
2023-07-17 13:14:06 -0300

    MAIL CONFIG

--- portafolio/core/views.py
+++ portafolio/core/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from django.conf import settings
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -6,3 +10,35 @@ def home(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def mail (request):
+    if request.method == 'POST':
+        first_name = request.POST = ['first_name']
+        last_name = request.POST = ['last_name']
+        contry = request.POST = ['contry']
+        city = request.POST = ['city']
+        email = request.POST = ['email']
+        message = request.POST = ['descripcion']
+        
+        template = render_to_string('email.template.html', {
+            'first_name': first_name,
+            'last:name': last_name,
+            'contry': contry,
+            'city': city,
+            'email': email,
+            'message':message
+        })
+        
+        email = EmailMessage(
+           template,
+           settings.EMAIL.HOST.USER,
+           ['rpalacioso12@outlook.com']
+            
+        )
+    
+        email.fail_silently = False
+        
+        email.send()
+
+        messages.success(request, 'Message sent')
+        return redirect('core/contact.html')
--- portafolio/portafolio/settings.py
+++ portafolio/portafolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +131,12 @@ import os
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.mail.outlook.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USERNAME = config ('EMAIL_HOST_USERNAME')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_USE_SLL = False
